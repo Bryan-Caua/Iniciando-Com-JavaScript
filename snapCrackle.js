373c5769a983664f13378cf023090b4b8a9cec25
Bryan <bryannarlok@gmail.com>
2022-05-04 14:26:44 -0300

    s5 - 02

---
+++ snapCrackle.js
@@ -0,0 +1,20 @@
+function snapCrackle(MaxValue) {
+  let resultado = []
+  for (let i = 1; i <= MaxValue; i++) {
+    if (i % 2 != 0 && i % 5 == 0) {
+        resultado.push('snapCrackle')
+    }
+    if (i % 2 != 0) {
+        resultado.push('snap')
+    }
+    if (i / 5 == 0) {
+        resultado.push('Crackle')
+    }
+    if (i % 2 == 0 && i / 5 != 0) {
+        resultado.push(i)
+    }
+  }
+  return resultado;
+}
+
+console.log(snapCrackle(150))
